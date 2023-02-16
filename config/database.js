77e7f3d386fc4568be0593a34cfbbbaeb638e78f
Jacob Broughton <jlbroughton88@gmail.com>
2023-02-16 10:35:42 -0500

    added turnary on database.js to determine environment variables

--- config/database.js
+++ config/database.js
@@ -3,15 +3,12 @@ const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 require("dotenv").config();
 
-console.log(process.env)
-
-
 const dbOptions = {
-  host:  process.env.DB_HOST_DEV,
-  port: process.env.DB_PORT_DEV,
-  user: process.env.DB_USER_DEV,
-  password: process.env.DB_PASSWORD_DEV,
-  database: process.env.DB_NAME_DEV,
+  host: process.env.NODE_ENV === 'development' ? process.env.DB_HOST_DEV : process.env.DB_HOST_PROD,
+  port: process.env.NODE_ENV === 'development' ? process.env.DB_PORT_DEV : process.env.DB_PORT_PROD,
+  user: process.env.NODE_ENV === 'development' ? process.env.DB_USER_DEV : process.env.DB_USER_PROD,
+  password: process.env.NODE_ENV === 'development' ? process.env.DB_PASSWORD_DEV : process.env.DB_PASSWORD_PROD,
+  database: process.env.NODE_ENV === 'development' ? process.env.DB_NAME_DEV : process.env.DB_NAME_PROD,
 };
 
 const connection = mysql.createConnection(dbOptions);
