a88c60628970ff002f249ea918d431d9fa185758
Jacob Broughton <jlbroughton88@gmail.com>
2023-02-19 11:23:54 -0500

    added res and req to auth middleware

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -8,6 +8,8 @@ module.exports.isAuth = function (req, res, next) {
         message: "You are not authorized",
         user: req.user,
         session: req.session,
+        req,
+        res,
       });
   }
 };
