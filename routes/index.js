2b7352e1b0c286413cc1897aac22f2c2d627dad9
Jacob Broughton <jlbroughton88@gmail.com>
2023-02-25 10:25:24 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const { body } = require("express-validator");
 
 router.get("/", passport.authenticate("local", {
   failureRedirect: "/failure",
-  failureFlash: true
 }), isAuth, (req, res) => {
   res.send({ message: "You're logged in", user: req.user });
 });
