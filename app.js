bb4f9bb186762a3128fce46841cb682c93eb832c
Jacob Broughton <jlbroughton88@gmail.com>
2023-02-25 11:49:51 -0500

    modified app.use session config

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const passport = require("passport");
 const session = require("express-session");
 require("dotenv").config();
-const { sessionStore } = require("./config/database.js");
 
 const app = express();
 
@@ -26,19 +24,25 @@ if (process.env.NODE_ENV === "production") {
   origin = "http://localhost:3000"
 }
 
+const passport = require("passport");
+const { sessionStore } = require("./config/database.js");
 app.use(cookieParser(process.env.SESSION_SECRET));
-
 app.use(
   session({
     store: sessionStore,
     secret: process.env.SESSION_SECRET,
-    saveUninitialized: false, // allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.
+    proxy: true,
+    saveUninitialized: true, // allows any uninitialized session to be sent to the store. When a session is created but not modified, it is referred to as uninitialized.
     resave: false, // enables the session to be stored back to the session store, even if the session was never modified during the request.
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // one day
+      secure: true,
+      httpOnly: false,
+      sameSite: 'none'
     },
   })
 );
+
 require("./config/passport"); // pretty much includes the passport.use()
 app.use(passport.initialize()); // initialize the middleware, makes sure it doesnt get stale
 app.use(passport.session()); // allows passport to plug into sessions table
