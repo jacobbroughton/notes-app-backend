6bcac2d85208b77198c498d2f4514486db138d4b
Jacob Broughton <jlbroughton88@gmail.com>
2023-02-25 09:01:12 -0500

    changed /login route

--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,29 @@ router.get("/", isAuth, (req, res) => {
 
 // passport.authenticate basically gives 'username' and 'password' and executes the verifyCallback function
 // Only continues past to the callback if authenticated with a user
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/",
-    failureFlash: true
-  }),
-  // If this function is called, auth was successful
-  (req, res) => {
-    console.log('Login route!  Authenticated?:', req.isAuthenticated(), ' Session:', req.session);
-    res.redirect('/')
-  }
-)
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+    if (err) {
+      res.status(500).json({ message: 'Something went wrong authenticating user' });
+      return;
+    }
+
+    if (!theUser) {
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, (err) => {
+      if (err) {
+        res.status(500).json({ message: 'Session save went bad.' });
+        return;
+      }
+      console.log('---123456789098765432345678---', req.user);
+      res.status(200).json({ errors: false, user: theUser });
+    });
+  })(req, res, next);
+});
 
 
 
