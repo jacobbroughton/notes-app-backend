11a1c45407c33674789c68d03b9340457f4664b4
Jacob Broughton <jlbroughton88@gmail.com>
2023-04-01 16:22:47 -0400

    test

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,4 +1,7 @@
 module.exports.isAuth = function (req, res, next) {
+  console.log("req.session", req.session)
+  console.log("req.user", req.user)
+  console.log("req.body", req.body)
   if (req.user) {
     next(); // moves on
   } else {
