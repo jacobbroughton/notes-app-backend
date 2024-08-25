0266b43e2aa1318896d6ab1efca9308098249fa6
Jacob Broughton <jlbroughton88@gmail.com>
2024-08-25 10:49:49 -0400

    trying connection string for prod

--- config/database.js
+++ config/database.js
@@ -10,31 +10,43 @@ dotEnv.config();
 console.log(process.env.NODE_ENV);
 
 const dbOptions = {
-  connectionLimit: 10,
-  host:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_HOST_DEV
-      : process.env.DB_HOST_PROD,
-  port:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_PORT_DEV
-      : process.env.DB_PORT_PROD,
-  user:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_USER_DEV
-      : process.env.DB_USER_PROD,
-  password:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_PASSWORD_DEV
-      : process.env.DB_PASSWORD_PROD,
-  database:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_NAME_DEV
-      : process.env.DB_NAME_PROD,
+  // connectionLimit: 10,
+  // host:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_HOST_DEV
+  //     : process.env.DB_HOST_PROD,
+  // port:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_PORT_DEV
+  //     : process.env.DB_PORT_PROD,
+  // user:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_USER_DEV
+  //     : process.env.DB_USER_PROD,
+  // password:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_PASSWORD_DEV
+  //     : process.env.DB_PASSWORD_PROD,
+  // database:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_NAME_DEV
+  //     : process.env.DB_NAME_PROD,
   // ssl: { rejectUnauthorized: process.env.NODE_ENV === 'development' ? false : true }
 };
 
-const pool = new Pool(dbOptions);
+const pool = new Pool(
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DB_CONNECTION_STRING_PROD,
+      }
+    : {
+        host: process.env.DB_HOST_DEV,
+        port: process.env.DB_PORT_DEV,
+        user: process.env.DB_USER_DEV,
+        password: process.env.DB_PASSWORD_DEV,
+        database: process.env.DB_NAME_DEV,
+      }
+);
 
 try {
   pool.connect();
@@ -50,7 +62,4 @@ const sessionStore = new pgSession({
   // Insert connect-pg-simple options here
 });
 
-export {
-  pool,
-  sessionStore,
-};
+export { pool, sessionStore };
