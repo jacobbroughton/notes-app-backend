a3aafc4e35b32185f44f4c6ffe398d71c9e67539
Jacob Broughton <jlbroughton88@gmail.com>
2024-08-25 18:35:33 -0400

    changed to connection string

--- config/database.js
+++ config/database.js
@@ -11,26 +11,27 @@ console.log(process.env.NODE_ENV);
 
 const dbOptions = {
   // connectionLimit: 10,
-  host:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_HOST_DEV
-      : process.env.DB_HOST_PROD,
-  port:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_PORT_DEV
-      : process.env.DB_PORT_PROD,
-  user:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_USER_DEV
-      : process.env.DB_USER_PROD,
-  password:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_PASSWORD_DEV
-      : process.env.DB_PASSWORD_PROD,
-  database:
-    process.env.NODE_ENV === "development"
-      ? process.env.DB_NAME_DEV
-      : process.env.DB_NAME_PROD,
+  // host:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_HOST_DEV
+  //     : process.env.DB_HOST_PROD,
+  // port:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_PORT_DEV
+  //     : process.env.DB_PORT_PROD,
+  // user:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_USER_DEV
+  //     : process.env.DB_USER_PROD,
+  // password:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_PASSWORD_DEV
+  //     : process.env.DB_PASSWORD_PROD,
+  // database:
+  //   process.env.NODE_ENV === "development"
+  //     ? process.env.DB_NAME_DEV
+  //     : process.env.DB_NAME_PROD,
+  connectionString: process.env.DB_CONNECTION_STRING_PROD,
   ssl: process.env.NODE_ENV === "development" ? false :{ rejectUnauthorized: false },
   min: 0,
   idleTimeoutMillis: 0,
@@ -41,7 +42,7 @@ const dbOptions = {
   // createRetryIntervalMillis: 100,
 };
 
-const pool = new Pool(dbOptions);
+const pool = new Pool({dbOptions});
 
 pool.on('error', (err) => {
   console.log("Encountered error", err)
